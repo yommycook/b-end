3f9cb0124591a244a2c6e0089e71bf2120e74e09
daniel <asa47972000@naver.com>
2023-04-25 20:56:17 +0900

    epxress

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+import express from "express"
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, (e) => {
+  if(e) console.log
+  else {
+    console.log(`app listening PORT - ${PORT}`);
+  }
+});
