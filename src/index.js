d60fcc3f21cf9770d3bd27bab8b28519f8a73dd8
daniel <asa47972000@naver.com>
2023-05-03 14:32:39 +0900

    add pug for test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import express from "express";
+import express from 'express';
+import path from 'path';
+import { login } from './login.js';
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+app.set('view engine', 'pug');
+app.set('views', path.join(process.cwd(), '/view'));
+app.post('/login', login);
+
+// 소스트리 -> 로컬  add existing something
+// git push --force
+
+app.get('/', (req, res, next) => {
+	res.render('index');
+});
 
 app.listen(PORT, (e) => {
-  console.log("hello world!", PORT)
-})
+	console.log('hello world!', PORT);
+});
---
+++ src/login.js
@@ -0,0 +1,6 @@
+
+
+export const login = (req, res, next) => {
+  
+
+}
