8e7c38e928ab98c1e9448dade5fe18390b12ab1e
daniel <asa47972000@naver.com>
2023-05-05 16:15:04 +0900

    practice react with auth db

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-import express from 'express';
-import path from 'path';
-import { login } from './login.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 
-const PORT = 3000;
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.set('view engine', 'pug');
-app.set('views', path.join(process.cwd(), '/view'));
-app.post('/login', login);
-
-// 소스트리 -> 로컬  add existing something
-// git push --force
-
-app.get('/', (req, res, next) => {
-	res.render('index');
-});
-
-app.listen(PORT, (e) => {
-	console.log('hello world!', PORT);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
--- src/login.js
+++
@@ -1,6 +0,0 @@
-
-
-export const login = (req, res, next) => {
-  
-
-}
